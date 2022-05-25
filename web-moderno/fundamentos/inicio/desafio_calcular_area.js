ada38af82ae83b363417e556ef8c07aae8c9c26e
Salumao Barbosa da Costa <salubcosta@gmail.com>
2022-05-25 17:41:51 -0300

    usando constante math.pi

---
+++ web-moderno/fundamentos/inicio/desafio_calcular_area.js
@@ -0,0 +1,10 @@
+const PI = 3.141592;
+let raio = 10;
+let areaCircunferencia = PI * raio * raio;
+
+
+console.log("O valor da área é " + areaCircunferencia +"m2");
+
+areaCircunferencia = Math.PI * raio * raio;
+
+console.log("O valor da área é " + areaCircunferencia.toFixed(4) +"m2");
