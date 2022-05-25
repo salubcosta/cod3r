cee0ed65a9e8f708beb17f28d3f0aefa3d8bc438
Salumao Barbosa da Costa <salubcosta@gmail.com>
2022-05-25 17:21:02 -0300

    Printando desconto na tela com duas casas decimais

--- web-moderno/fundamentos/inicio/04_variaveis.js
+++ web-moderno/fundamentos/inicio/04_variaveis.js
@@ -3,9 +3,12 @@ var quantidade = 10;
 var preco = 6.4;
 var imposto = 1.5;
 var precoFinal = preco + imposto;
+let desconto = 0.1;
 
 console.log(produto);
 console.log(quantidade);
 console.log(preco);
 console.log(imposto);
 console.log(precoFinal);
+
+console.log("Desconto: " +  (preco * (1 - desconto)).toFixed(2));
