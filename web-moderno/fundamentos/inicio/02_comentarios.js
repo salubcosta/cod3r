97e8b4943a4499fdba00eec5441b91983575c68a
Salumao Barbosa da Costa <salubcosta@gmail.com>
2022-05-25 06:16:43 -0300

    formas de comentar no js

---
+++ web-moderno/fundamentos/inicio/02_comentarios.js
@@ -0,0 +1,12 @@
+/**
+ * Este é um tipo de comentário em várias linhas
+ */
+
+// Este é um tipo de comentário em apenas 1 linha
+
+
+/**
+ * Exemplo de código comentado:
+ */
+
+// console.log('comando ignorado...');
