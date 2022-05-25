d1a7c6ff38103f6c4970e42c4a2093b62a78e9ea
Salumao Barbosa da Costa <salubcosta@gmail.com>
2022-05-25 12:07:21 -0300

    bloco de códigos

---
+++ web-moderno/fundamentos/inicio/03_bloco.js
@@ -0,0 +1,11 @@
+// Bloco de código
+{
+    console.log("Passo #01");
+    console.log("Passo #02");
+    console.log("Passo #03");
+}
+
+{
+    console.log("Passo #04");
+    console.log("Passo #05");
+}
